76a8f8afa2e308831913cf2ee4e467b6cd99b640
adamapure <adamapurechetan@gmail.com>
2022-12-26 19:09:35 +0530

    remove ';'for code

--- FaceBookLogin/UnitTest1.cs
+++ FaceBookLogin/UnitTest1.cs
@@ -22,7 +22,7 @@ namespace FaceBookLogin
         {
             driver.Navigate().GoToUrl("https://www.facebook.com/");
             //  driver.FindElement(By.XPath("//input[@name='email']")).SendKeys("8605227616");
-            //  driver.FindElement(By.XPath("//input[@name='pass']")).SendKeys("Chetu@0707");
+            //  driver.FindElement(By.XPath("//input[@name='pass']")).SendKeys("Chetu@0707")
             // driver.FindElement(By.XPath("//button[@name='login']")).Click();
             // driver.FindElement(By.CssSelector("#login_link > div._xkt > a")).Click()
             IWebElement createnewAccount = driver.FindElement(By.CssSelector("#u_0_0_eI"));
